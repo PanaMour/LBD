c3b63d738a9d1ab9ccd465bedbc52ce42e059b18
PanaMour <panamour25@gmail.com>
2025-12-24 23:42:50 +0200

    Added Main Menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Mirror;
+using TMPro;
+
+public class MainMenuController : MonoBehaviour
+{
+    public NetworkManager manager;
+    public TMP_InputField ipInputField;
+
+    public void HostDuel()
+    {
+        manager.StartHost();
+    }
+
+    public void JoinDuel()
+    {
+        manager.networkAddress = ipInputField.text;
+        manager.StartClient();
+    }
+}
