c811d2a80e5e267b3777b03d4b1c8d67d70eff6a
PanaMour <panamour25@gmail.com>
2026-02-10 20:54:23 +0200

    Fixed PlayAgain

--- Assets/Scripts/PlayAgain.cs
+++ Assets/Scripts/PlayAgain.cs
@@ -9,12 +9,34 @@ public class PlayAgain : MonoBehaviour
     public GameObject textObject;
     public GameObject playAgainButton;
 
+    public string menuSceneName = "MainMenu";
+
     public void RestartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        PlayerLP.staticLP = 1;
-        OpponentLP.staticLP = 1;
+        PlayerLP.staticLP = 4000;
+        OpponentLP.staticLP = 4000;
+
         playAgainButton.SetActive(false);
         textObject.SetActive(false);
+
+        if (NetworkManager.singleton != null)
+        {
+            if (NetworkServer.active && NetworkClient.isConnected)
+            {
+                NetworkManager.singleton.StopHost();
+            }
+            else if (NetworkClient.isConnected)
+            {
+                NetworkManager.singleton.StopClient();
+            }
+            else if (NetworkServer.active)
+            {
+                NetworkManager.singleton.StopServer();
+            }
+        }
+        else
+        {
+            SceneManager.LoadScene(menuSceneName);
+        }
     }
 }
